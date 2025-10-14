be5da08c7ace7edec0f2a464ff56f8ada700db0c
Khaled Algharieb <kalgharieb@gmail.com>
2025-10-14 16:05:10 +0200

    refactor v1.5

--- main.tf
+++ main.tf
@@ -1,48 +1,48 @@
 
 
 provider "azurerm" {
-    features {}
-    use_oidc = true
+  features {}
+  use_oidc = true
 }
 
 resource "azurerm_resource_group" "main" {
-    name     = "rg-kgeek-tst"
-    location = "West Europe"
-  tags = { 
-  Environment = "Testing"
-  Owner = "kgeek"
-  Project = "Terraform"
-}
+  name     = "rg-kgeek-tst"
+  location = "West Europe"
+  tags = {
+    Environment = "Testing"
+    Owner       = "kgeek"
+    Project     = "Terraform"
+  }
 }
 
 resource "azurerm_virtual_network" "kgeek_tst" {
-    name                = "kgeek-tst"
-    address_space       = ["172.16.0.0/16"]
-    location            = azurerm_resource_group.main.location
-    resource_group_name = azurerm_resource_group.main.name
+  name                = "kgeek-tst"
+  address_space       = ["172.16.0.0/16"]
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
 }
 
 resource "azurerm_subnet" "sub1" {
-    name                 = "sub1"
-    resource_group_name  = azurerm_resource_group.main.name
-    virtual_network_name = azurerm_virtual_network.kgeek_tst.name
-    address_prefixes     = ["172.16.1.0/24"]
+  name                 = "sub1"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.kgeek_tst.name
+  address_prefixes     = ["172.16.1.0/24"]
 }
 
 resource "azurerm_subnet" "sub2" {
-    name                 = "sub2"
-    resource_group_name  = azurerm_resource_group.main.name
-    virtual_network_name = azurerm_virtual_network.kgeek_tst.name
-    address_prefixes     = ["172.16.2.0/24"]
+  name                 = "sub2"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.kgeek_tst.name
+  address_prefixes     = ["172.16.2.0/24"]
 }
 
 resource "azurerm_subnet" "sub3" {
-    name                 = "sub3"
-    resource_group_name  = azurerm_resource_group.main.name
-    virtual_network_name = azurerm_virtual_network.kgeek_tst.name
-    address_prefixes     = ["172.16.3.0/24"]
+  name                 = "sub3"
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.kgeek_tst.name
+  address_prefixes     = ["172.16.3.0/24"]
 }
 
 output "vnet_id" {
-    value = azurerm_virtual_network.kgeek_tst.id
+  value = azurerm_virtual_network.kgeek_tst.id
 }
--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,17 @@
 terraform {
-    required_providers {
-        azurerm = {
-            source  = "hashicorp/azurerm"
-            version = "~> 3.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
     }
+  }
 
-    backend "azurerm" {
-        resource_group_name  = "rg-tf-state"
-        storage_account_name = "alparslanterraformstate"
-        container_name       = "tfstate"
-        key                  = "terraform.tfstate"
-        use_oidc = true
-        use_azuread_auth = true
-    }
+  backend "azurerm" {
+    resource_group_name  = "rg-tf-state"
+    storage_account_name = "alparslanterraformstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+    use_azuread_auth     = true
+  }
 }
