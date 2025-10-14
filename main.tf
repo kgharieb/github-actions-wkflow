90c9463f75775d77aed397cd5f24ff7a555522fb
Khaled Algharieb <kalgharieb@gmail.com>
2025-10-14 16:18:23 +0200

    create vm

--- main.tf
+++ main.tf
@@ -43,6 +43,42 @@ resource "azurerm_subnet" "sub3" {
   address_prefixes     = ["172.16.3.0/24"]
 }
 
+resource "az_vm" "example" {
+  name                  = "alpaslan-machine"
+  location              = azurerm_resource_group.main.location
+  resource_group_name   = azurerm_resource_group.main.name
+  network_interface_ids = [azurerm_network_interface.example.id]
+  subnet_id             = azurerm_subnet.sub1.id
+  vm_size               = "Standard_DS1_v2"
+
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+
+  storage_os_disk {
+    name              = "example-os-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = "adminuser"
+    admin_password = "Password1234!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
+}
+
 output "vnet_id" {
   value = azurerm_virtual_network.kgeek_tst.id
 }
