59630a8e711875f393543a7fe1525d9423935409
Khaled Algharieb <kalgharieb@gmail.com>
2025-10-16 14:24:12 +0200

    refix bk

--- terraform.tf
+++ terraform.tf
@@ -5,13 +5,13 @@ terraform {
       version = "~> 3.0"
     }
   }
+}
 
-  backend "azurerm" {
+  /*backend "azurerm" {
     resource_group_name  = "rg-tf-state"
     storage_account_name = "alparslanterraformstate"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_oidc             = true
     use_azuread_auth     = true
-  }
-}
+  }*/
